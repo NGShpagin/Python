f698aa34ac766d44c0cec15d779dbec0566c0046
NGShpagin <shpagin9393@mail.ru>
2022-07-14 23:48:03 +0300

    Hometask_3 done

--- Seminar_2/Hometask_3.py
+++ Seminar_2/Hometask_3.py
@@ -3,8 +3,30 @@
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 
-my_list = input('Введите значения для списка: ').split()
+# number = int(input('Введите число  = ')) 
+# sum = 0 
+# x = 0 
+# my_list = [] 
+# print('{', end = ' ') 
+# for i in range(1,n + 1): 
+#     x = round((1+1/i)**i,2) 
+#     print( i,':', x,',',end = ' ') 
+#     my_list.append(x) 
+#     sum = sum + x 
+   
+# print ('}') 
+# print('Cумма чисел последовательности = ',sum) 
+# print(my_list)
+
+
+num = int(input('Введите число: '))
+suc = round((1+ 1/num)**num)
+my_list = [1+suc]
 sum = 0
+while len(my_list) < num:
+    my_list.append(my_list[-1]+suc)
 for i in my_list:
-    sum += (1+1/int(i))**int(i)
-print(round(sum, 2))
+    sum += i
+print(f'Последовательность: (1+1/{num})^{num} = ', (round((1+ 1/6)**6)), '(округлена до целого числа)')
+print(my_list)
+print('Сумма всех элементов в списке: ', sum)
